8d293234b568b9e462233b59de5675add3543c5f
Jonny Reading <jonny@firefinch.io>
2025-06-19 13:44:59 +0100

    Constant for 60fps tick interval

--- Constants.cs
+++ Constants.cs
@@ -4,4 +4,9 @@ public static class Constants
 {
     public const string AppIdentifier = "com.jonnyreading.audioswitch";
     public const string AppName = "AudioSwitch";
+
+    /// <summary>
+    /// Approximates 60fps
+    /// </summary>
+    public const int TickInterval = 1000 / 60;
 }
