276cf5b4cb5a084d7dac13663f94b7316099970e
Jonny Reading <jonny@firefinch.io>
2025-06-19 12:21:21 +0100

    Theme description enum

---
+++ Enum/Theme.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace AudioSwitch.Enum;
+
+[JsonConverter(typeof(StringEnumConverter))]
+public enum Theme
+{
+    Dark,
+    Light,
+    System
+}
